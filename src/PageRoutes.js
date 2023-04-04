b84b3d53319cf0f54c37458dca4829cd99cfabfd
bastocrown <bastocrown@gmail.com>
2023-04-03 22:39:36 -0700

    add: create risk page

--- src/PageRoutes.js
+++ src/PageRoutes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Navigate, Routes, BrowserRouter } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import CreaetRisk from "./pages/CreaetRisk";
 
 const PageRoutes = (props) => {
   return (
@@ -10,6 +11,8 @@ const PageRoutes = (props) => {
           <Routes>
               <Route exact path="/" element={<Navigate to="/dashboard"/>}></Route>
               <Route path="/dashboard" element={<Dashboard /> } />
+              <Route path="/create-risk" element={<CreaetRisk /> } />
+              
           </Routes>
           </BrowserRouter>
         </div>
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,6 +4,12 @@ const Dashboard = (props) => {
     return (
         <div>
             <h1>Dashboard page</h1>
+            <p>Create Project Brief</p>
+            <p>Project Tasks</p>
+            <p>Resource Cards</p>
+            <p>Risk Pool</p>
+            <p>Emails</p>
+            <button>Start</button>
         </div>
     );
 }
